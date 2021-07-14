b8a4972e35d2855467f1294752153ed7575bef40
miguelfs96 <fersanmi908@gmail.com>
2021-07-14 17:16:38 +0200

    autogenerate(IJ), hot swap (IJ) & change languaje

--- keyboards/crkbd/keymaps/stewie_v1/keycodes.h
+++ keyboards/crkbd/keymaps/stewie_v1/keycodes.h
@@ -11,3 +11,7 @@ enum layers {
 #define RAISE MO(_RAISE)
 #define FUNCT MO(_FUNCT)
 
+#define AUT_GEN A(KC_INS) //alt + insert autogenerado intellij
+#define HOT_SW C(KC_F9)   //ctrl + f9 hot-swap intellij
+
+#define CH_LAN A(KC_LSFT) //alt + shift cambio idioma windows
--- keyboards/crkbd/keymaps/stewie_v1/keymap.c
+++ keyboards/crkbd/keymaps/stewie_v1/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,  KC_DEL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT,   KC_UP, KC_DOWN,KC_RIGHT, KC_SCLN, XXXXXXX,
+      KC_LCTL,  KC_DEL, XXXXXXX, XXXXXXX,  HOT_SW, AUT_GEN,                      KC_LEFT,   KC_UP, KC_DOWN,KC_RIGHT, KC_SCLN, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR,                      XXXXXXX, XXXXXXX, KC_COMM, KC_DOT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  CH_LAN,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
