fd7f6f6aff1b6e4a39b47c09d1b5a8be0d15c63f
miguelfs96 <fersanmi908@gmail.com>
2021-07-19 22:53:28 +0200

    Cambios en teclas

--- keyboards/crkbd/keymaps/stewie_v1/keycodes.h
+++ keyboards/crkbd/keymaps/stewie_v1/keycodes.h
@@ -11,7 +11,10 @@ enum layers {
 #define RAISE MO(_RAISE)
 #define FUNCT MO(_FUNCT)
 
+#define ALT_A MT(MOD_RALT, KC_A) //funcion como alt cuando se mantiene pulsado y como TAB al pulsar una vez
+
 #define AUT_GEN A(KC_INS) //alt + insert autogenerado intellij
 #define HOT_SW C(KC_F9)   //ctrl + f9 hot-swap intellij
+#define RST_SRV S(KC_F9)  //shif + f9 start or restart debug intellij
 
 #define CH_LAN A(KC_LSFT) //alt + shift cambio idioma windows
